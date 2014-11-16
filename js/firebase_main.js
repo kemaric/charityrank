ed87a810e236999579b5d6cbee10d73c9dbd2498
kemaric <kemaric@gmail.com>
2014-11-16 11:20:51 -0500

    Yaaay

--- js/firebase_main.js
+++ js/firebase_main.js
@@ -1,11 +1,15 @@
  $(document).ready(function(){
   var URL = 'https://chariyrank.firebaseio.com/Charity';
  var myCharityDataRef = new Firebase(URL);
+
       var categories = retrieveValues(myCharityDataRef,"Category");
       var matchedCategories = [];
      // var charietyKids = myCharityDataRef;
       $('#categoryInput').keypress(function (e) {
         if (e.keyCode == 13) {
+
+        //ranker for items
+        var ranker =  {};
          var category = $('#categoryInput').val();
         myCharityDataRef.orderByChild("Category").equalTo(category).on("child_added", function(matchesnap){
          //if there is a result
@@ -22,7 +26,11 @@
         }, 2000);
         
          var locationReg = "[A-Za-z ]*, [A-Z]{2}";
-         randomCharity = getRandomItems(matchedCategories,2);
+
+         //the bracket
+         var bracket = getRandomItems(matchedCategories,8);
+
+         randomCharity = getRandomItems(bracket,2);
          myCharityDataRef.orderByKey().equalTo(randomCharity[0]).on("child_added", function(item){
           var fld = item.val();
          
@@ -70,16 +78,23 @@
             var fld = item.val();
             if (buttonTitle == fld["Title"]){
             // charity 1 won
-              matchedCategories.pop(randomCharity[1]);
+             var looser =  bracket.pop(randomCharity[1]);
+              ranker = addToRanker(ranker, randomCharity[0]);
+              if(bracket.length  > 2){             
+                 ranker = removeFromRanker(ranker, looser);
+            }
             } else {
             // charity 2 one
-              matchedCategories.pop(randomCharity[0]);
+              var looser = bracket.pop(randomCharity[0]);
+              ranker = addToRanker(ranker, randomCharity[1]);
+              ranker = removeFromRanker(ranker, looser);
             }
 
           });
            
-         var locationReg = "[A-Za-z ]*, [A-Z]{2}";
-         randomCharity = getRandomItems(matchedCategories,2);
+           if(bracket.length > 1){
+            var locationReg = "[A-Za-z ]*, [A-Z]{2}";
+         randomCharity = getRandomItems(bracket,2);
          myCharityDataRef.orderByKey().equalTo(randomCharity[0]).on("child_added", function(item){
           var fld = item.val();
          
@@ -121,8 +136,32 @@
           }
 
 
-            //getRandomItems
+            
           });
+         }else{
+              $("body").empty();
+              $("body").html("<div id=\"header\" class=\"text-center\"><h2>Your top 3 results:<\/h2><!-- Main --><div id=\"main\"><header class=\"major container 75%\"> <ol><li><p id=\"Title1\">Title1<\/p><\/li><li><p id=\"Title2\">Title2<\/p><\/li><li><p id=\"Title3\">Title3<\/p><\/li><\/ol><\/header><a id=\"buttonTitle2\" class=\"button\" href=\"/communityranks.html\">Go see the community's results<\/a></div>");
+            var keysSorted = Object.keys(ranker).sort(function(a,b){return ranker[a]-ranker[b]});
+
+             myCharityDataRef.orderByKey().equalTo(keysSorted[0]).on("child_added", function(item){
+                var fld = item.val();
+                console.log(fld);
+                $("#Title1").html(fld["Title"]);
+             });
+            myCharityDataRef.orderByKey().equalTo(keysSorted[1]).on("child_added", function(item){
+                            var fld = item.val();
+                            console.log(fld);
+                            $("#Title2").html(fld["Title"]);
+                         });
+            myCharityDataRef.orderByKey().equalTo(keysSorted[2]).on("child_added", function(item){
+                            var fld = item.val();
+                            console.log(fld);
+                            $("#Title3").html(fld["Title"]);
+                         });
+
+
+         }
+         
         });
          });
           
@@ -242,3 +281,116 @@ function calcElo(p1beatp2,p1wins,p1loses,p2wins,p2loses) {
     p2wins: p2wins,
     p2loses: p2loses}
 };
+
+function winvsloss(p1beatp2, p1, p2) {
+    //updating the winner's winning score
+    //TODO : Add in  a transcation to prevent data races
+    var p1Ref = new Firebase("https://chariyrank.firebaseio.com/chariyrank/" + p1);
+    var p2Ref = new Firebase("https://chariyrank.firebaseio.com/chariyrank/" + p2);
+    var p1win2, p1loses, p1elo, p2wins, p2loses, p2elo;
+
+
+
+    if (p1beatp2) {
+        //setting the winner score
+        p1wins = 1;
+        p2loses = 1;
+
+        //setting server data
+        p1Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+            p1elo = fld.elo;
+            p1wins += fld.wins;
+            p1loses = fld.loses;
+            //calclate new elo
+            // fld.elo = (p1elo+400*(fld.wins-p1loses))/(p1wins+p1loses);
+            //set new data
+            // itemRef.set(fld);
+        });
+
+        //setting the loser score
+
+        p2Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+
+            p2elo = fld.elo;
+            p2wins = fld.wins;
+            p2loses += fld.loses;
+            fld.elo = (p1elo + 400 * (p2wins - p2loses)) / (p2wins + p2loses);
+            fld.loses = p2loses;
+            p2Ref.set(fld);
+        });
+
+        //updating p1 stuff
+        p1Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+            fld.elo = (p2elo + 400 * (p1wins - p1loses)) / (p1wins + p1loses);
+            fld.wins = p1wins;
+            p1Ref.set(fld);
+        });
+
+
+    } else {
+        //setting p2 to winner
+        p2wins = 1;
+        p1loses = 1;
+        //setting p1 to loser
+
+        //setting server data
+        p1Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+            p1elo = fld.elo;
+            p1wins = fld.wins;
+            p1loses += fld.loses;
+            //calclate new elo
+            // fld.elo = (p1elo+400*(fld.wins-p1loses))/(p1wins+p1loses);
+            //set new data
+            // itemRef.set(fld);
+        });
+
+        //setting the loser score
+
+        p2Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+
+            p2elo = fld.elo;
+            p2wins += fld.wins;
+            p2loses = fld.loses;
+            fld.elo = (p1elo + 400 * (p2wins - p2loses)) / (p2wins + p2loses);
+            fld.wins = p2wins;
+            p2Ref.set(fld);
+        });
+
+        //updating p1 stuff
+        p1Ref.once("value", function(item) {
+            var fld = item.val();
+            console.log(fld);
+            fld.elo = (p2elo + 400 * (p1wins - p1loses)) / (p1wins + p1loses);
+            fld.loses = p1loses;
+            p1Ref.set(fld);
+        });
+
+    }
+
+};
+
+function addToRanker(ranker, elem){
+  if(elem in ranker && ranker[elem] != null){
+    ranker[elem] = ranker[elem] + 1;
+  }else{
+    ranker[elem] = 1;
+  }
+  return ranker;
+}
+
+function removeFromRanker(ranker, elem){
+  if(elem in ranker && ranker[elem] != null){
+    ranker[elem] = null;
+  }
+  return ranker;
+}
